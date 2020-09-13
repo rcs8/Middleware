6cef16be46899761a5ac75ea96dc9f77240d9a84
Rodrigo Cunha <rodrigo.cunha@manifestohut.com.br>
2020-09-13 12:03:39 -0300

    remove shared functions from servers

--- Exercicio 2/socket/server-tcp.go
+++ Exercicio 2/socket/server-tcp.go
@@ -3,7 +3,6 @@ package socket
 import (
 	"encoding/json"
 	"fmt"
-	"math"
 	"net"
 
 	"../shared"
@@ -57,32 +56,4 @@ func HandleTCP(conn net.Conn) {
 			fmt.Println(err)
 		}
 	}
-}
-
-func InvokeSqrt(args shared.Args) shared.Reply {
-	var a = float64(args.A)
-	var b = float64(args.B)
-	var c = float64(args.C)
-
-	deltaValue := CalculateDelta(a, b, c)
-
-	if deltaValue < 0 {
-		return shared.Reply{
-			Result: "Nenhuma raiz real\n",
-		}
-	}
-
-	if deltaValue == 0 {
-		return shared.Reply{
-			Result: fmt.Sprintf("%f\n", (b*(-1))/(2*a)),
-		}
-	}
-
-	return shared.Reply{
-		Result: fmt.Sprintf("%f e %f\n", (math.Sqrt(deltaValue)-b)/2*a, ((-1)*math.Sqrt(deltaValue)-b)/2*a),
-	}
-}
-
-func CalculateDelta(a, b, c float64) float64 {
-	return (b * b) - (4 * a * c)
 }
--- Exercicio 2/socket/server-udp.go
+++ Exercicio 2/socket/server-udp.go
@@ -3,7 +3,6 @@ package socket
 import (
 	"encoding/json"
 	"fmt"
-	"math"
 	"net"
 
 	"../shared"
@@ -64,32 +63,4 @@ func HandleUDP(conn *net.UDPConn, msgFromClient []byte, n int, addr *net.UDPAddr
 	if err != nil {
 		fmt.Println(err)
 	}
-}
-
-func InvokeSqrt(args shared.Args) shared.Reply {
-	var a = float64(args.A)
-	var b = float64(args.B)
-	var c = float64(args.C)
-
-	deltaValue := CalculateDelta(a, b, c)
-
-	if deltaValue < 0 {
-		return shared.Reply{
-			Result: "Nenhuma raiz real\n",
-		}
-	}
-
-	if deltaValue == 0 {
-		return shared.Reply{
-			Result: fmt.Sprintf("%f\n", (b*(-1))/(2*a)),
-		}
-	}
-
-	return shared.Reply{
-		Result: fmt.Sprintf("%f e %f\n", (math.Sqrt(deltaValue)-b)/2*a, ((-1)*math.Sqrt(deltaValue)-b)/2*a),
-	}
-}
-
-func CalculateDelta(a, b, c float64) float64 {
-	return (b * b) - (4 * a * c)
 }
