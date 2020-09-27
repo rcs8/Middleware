c167a29ec7d23b2ef69d33ea59ce2e8cbf617140
Rodrigo Cunha <rodrigo.cunha@manifestohut.com.br>
2020-09-27 17:09:03 -0300

    impl names service and register new sqrt

--- Exercicio 3/server-tcp.go
+++ Exercicio 3/server-tcp.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net"
+	"net/rpc"
 	"time"
 )
 
 type ServerTCP struct {
-	listener *net.Listener
+	listener  *net.Listener
+	serverRPC *rpc.Server
 }
 
 func NewServerTCP(address string) (*ServerTCP, error) {
+	sqrt := new(SqrtRPC)
+
+	server := rpc.NewServer()
+	server.RegisterName("Sqrt", sqrt)
+
 	ln, err := net.Listen("tcp", address)
-	if err != nil {
-		return nil, err
-	}
 
 	return &ServerTCP{
-		listener: &ln,
+		listener:  &ln,
+		serverRPC: server,
 	}, err
 }
 
@@ -26,45 +29,11 @@ func (s *ServerTCP) ListenTCP(exit NotifChan, exited NotifChan) {
 	listener := (*s.listener).(*net.TCPListener)
 	for {
 		listener.SetDeadline(time.Now().Add(1 * time.Second))
-		conn, err := listener.Accept()
-		if err != nil {
-			_, stop := <-exit
-			if stop {
-				listener.Close()
-				exited <- true
-				return
-			}
-			continue
-		}
 
-		go HandleTCP(conn)
+		s.serverRPC.Accept(listener)
 	}
 }
 
 func (s *ServerTCP) Close() {
 	(*s.listener).Close()
-}
-
-func HandleTCP(conn net.Conn) {
-	var messageFromClient Args
-
-	defer conn.Close()
-
-	jsonDecoder := json.NewDecoder(conn)
-	jsonEncoder := json.NewEncoder(conn)
-
-	for {
-		err := jsonDecoder.Decode(&messageFromClient)
-		if err != nil && err.Error() == "EOF" {
-			conn.Close()
-			break
-		}
-
-		msgToClient := InvokeSqrt(messageFromClient)
-
-		err = jsonEncoder.Encode(msgToClient)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
 }
